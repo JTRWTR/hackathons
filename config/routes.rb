5204d71b51950dc686e6cb23b221c21f4d5e2ce5
okano0120 <okano0120@gmail.com>
2017-04-09 21:02:38 +0900

    トップ画面 作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  root 'top#index'
+  get 'top/index'
+
+  get 'top/ranking'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
