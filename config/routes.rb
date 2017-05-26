48e10a47a7926929957b46cb805bde539b2eb745
okano0120 <okano0120@gmail.com>
2017-05-26 15:18:42 +0900

    ハッカソン用編集

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,6 @@ Rails.application.routes.draw do
 
   get "home/index"
 
-  get '/auth/:provider/callback', :to => 'sessions#callback'
-
-  post '/auth/:provider/callback', :to => 'sessions#callback'
-
-  get '/logout' => 'sessions#destroy', :as => :logout
-
   get 'top/index'
 
   get 'top/ranking'
