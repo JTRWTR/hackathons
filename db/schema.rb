00b87f86e59965b253493d8926a347c7c2532a12
KanekoYuto <kanekoyuuto@gmail.com>
2017-05-27 14:35:18 +0900

    記事投稿

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,64 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170409130610) do
+ActiveRecord::Schema.define(version: 20170527051801) do
+
+  create_table "ar_internal_metadata", primary_key: "key", force: :cascade do |t|
+    t.string   "value",      limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
+
+  create_table "articles", force: :cascade do |t|
+    t.string   "title",          limit: 255
+    t.string   "thumbnail_path", limit: 255
+    t.string   "url",            limit: 255
+    t.integer  "time",           limit: 4
+    t.string   "site_url",       limit: 255
+    t.integer  "count",          limit: 4
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+  end
+
+  create_table "implessions", force: :cascade do |t|
+    t.string   "tracking_code", limit: 255
+    t.integer  "movie_id",      limit: 4
+    t.integer  "value",         limit: 4
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
+  end
+
+  create_table "keywords", force: :cascade do |t|
+    t.string   "name",       limit: 255
+    t.integer  "type",       limit: 4
+    t.integer  "count",      limit: 4
+    t.string   "url",        limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
+
+  create_table "movie_tags", force: :cascade do |t|
+    t.integer  "movie_id",   limit: 4
+    t.integer  "tag_id",     limit: 4
+    t.datetime "created_at",           null: false
+    t.datetime "updated_at",           null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "title",       limit: 255
+    t.integer  "keyword_id",  limit: 4
+    t.string   "description", limit: 255
+    t.string   "sentence",    limit: 255
+    t.integer  "count",       limit: 4
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string   "name",       limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "provider",    limit: 255
